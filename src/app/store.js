8849cc616d71ffc8ec9e7ef4f08a35cac369fa24
nishant7898 <nishantpathak271@gmail.com>
2025-07-27 12:24:06 +0530

    made responsive

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 // src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../Redux/AuthSlice.js';     
-import cartReducer from '../Redux/Cartslice.js';      
-import productReducer from '../Redux/ProductSlice.js'; 
+import authReducer from '../Redux/AuthSlice.jsx';     
+import cartReducer from '../Redux/Cartslice.jsx';      
+import productReducer from '../Redux/ProductSlice.jsx'; 
 
 const store = configureStore({
   reducer: {
